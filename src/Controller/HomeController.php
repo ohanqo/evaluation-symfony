ff585b455d7b0009a86f86dd8733fbd6281c15a0
Antoine <gm.anqo@gmail.com>
2019-12-06 16:22:33 +0100

    feat(home): index

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArtworkRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(ArtworkRepository $artworkRepository)
+    {
+        $artworks = $artworkRepository->findBy([], null, 4);
+
+        return $this->render('home/index.html.twig', [
+            'artworks' => $artworks,
+        ]);
+    }
+}
