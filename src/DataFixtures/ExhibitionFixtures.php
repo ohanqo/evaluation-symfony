d9e306bb523fecff0f56430ead771997a32645b4
Antoine <gm.anqo@gmail.com>
2019-12-06 10:59:58 +0100

    feat(exhibition): add fixtures

--- src/DataFixtures/ExhibitionFixtures.php
+++ src/DataFixtures/ExhibitionFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Exhibition;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory as Faker;
 
 class ExhibitionFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Faker::create('fr_FR');
+
+        for ($i = 0; $i < 10; $i++) {
+            $exhibition = new Exhibition();
+            $exhibition->setName($faker->city);
+            $exhibition->setPresentation($faker->text);
+
+            if ($i < 5) {
+                $completedDate = $faker->dateTimeBetween('-3 years', 'now');
+                $exhibition->setDate($completedDate);
+            } else {
+                $upcomingDate = $faker->dateTimeBetween('now', '+2 years');
+                $exhibition->setDate($upcomingDate);
+            }
+
+            $manager->persist($exhibition);
+        }
 
         $manager->flush();
     }
